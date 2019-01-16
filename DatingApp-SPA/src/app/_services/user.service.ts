98a15a8007c882ab6ebdf3ade4885f490a39e19c
andrea tosato <tosatoandrea@yahoo.it>
2019-01-16 17:56:07 +0100

    End of section 9

---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseUrl = environment.apiUr;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.baseUrl + 'users');
+  }
+
+  getUser(id: string): Observable<User> {
+    return this.httpClient.get<User>(this.baseUrl + 'users/' + id);
+  }
+}
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,9 +1,12 @@
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
     { path: '',     component: HomeComponent },
@@ -16,7 +19,8 @@ export const appRoutes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            { path: 'members',  component: MemberListComponent },
+            { path: 'members',  component: MemberListComponent, resolve: {users: MemberListResolver} },
+            { path: 'members/:id',  component: MemberDetailComponent, resolve: {user: MemberDetailResolver} },
             { path: 'messages', component: MessagesComponent },
             { path: 'lists',    component: ListsComponent }
         ]
